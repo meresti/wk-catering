31e072802c2160ed16f69e3af0fc94afd5a90899
zsolt.horvat <zsolt.horvat@gmail.com>
2014-03-16 12:55:37 +0000

    change password only if the current password is provided and matches the saved one

--- src/main/java/org/catering/web/login/PasswordBean.java
+++ src/main/java/org/catering/web/login/PasswordBean.java
@@ -5,7 +5,6 @@ package org.catering.web.login;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 import javax.enterprise.context.RequestScoped;
 import org.jasypt.util.password.ConfigurablePasswordEncryptor;
 
@@ -15,12 +14,20 @@ public class PasswordBean {
     private static final String DIGEST_ALGORITHM = "SHA-256";
 
     public String encryptPassword(String value) {
+        final ConfigurablePasswordEncryptor passwordEncryptor = createPasswordEncryptor();
+        return passwordEncryptor.encryptPassword(value);
+    }
 
+    private ConfigurablePasswordEncryptor createPasswordEncryptor() {
         final ConfigurablePasswordEncryptor passwordEncryptor = new ConfigurablePasswordEncryptor();
         passwordEncryptor.setPlainDigest(true);
         passwordEncryptor.setAlgorithm(DIGEST_ALGORITHM);
         passwordEncryptor.setStringOutputType("hexadecimal");
-        final String encrypted = passwordEncryptor.encryptPassword(value);
-        return encrypted;
+        return passwordEncryptor;
+    }
+
+    public boolean checkPassword(String plainPassword, String encryptedPassword) {
+        final ConfigurablePasswordEncryptor passwordEncryptor = createPasswordEncryptor();
+        return passwordEncryptor.checkPassword(plainPassword, encryptedPassword);
     }
 }
